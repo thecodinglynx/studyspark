929539a0c08d1461d37143e7e7efe18bd7172ffa
Lynxcoding <nvrmnd.lynxcoding@manyme.com>
2025-10-13 11:38:51 -0400

    feat: add neon database

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,28 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { neonConfig } from "@neondatabase/serverless";
+import ws from "ws";
 
 declare global {
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
-export const prisma = global.prisma ?? new PrismaClient();
+const connectionString = process.env.DATABASE_URL;
+
+const isNeon = connectionString?.includes(".neon.tech");
+
+let prismaClient: PrismaClient;
+
+if (isNeon && connectionString) {
+  neonConfig.webSocketConstructor = ws;
+  const adapter = new PrismaNeon({ connectionString });
+  prismaClient = global.prisma ?? new PrismaClient({ adapter } as any);
+} else {
+  prismaClient = global.prisma ?? new PrismaClient();
+}
+
+export const prisma = prismaClient;
 
 if (process.env.NODE_ENV !== "production") {
   global.prisma = prisma;
